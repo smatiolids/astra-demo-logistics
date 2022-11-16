b2f614e45c98e887a15719701ddb31764e2f2009
Samuel Matioli <smatioli@gmail.com>
2022-11-16 18:53:34 -0300

    Adding Constants

---
+++ streaming-functions/java/streamingfunction/src/test/java/com/astra/logisticsdemo/LoggingFunction.java
@@ -0,0 +1,20 @@
+package com.astra.logisticsdemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class LoggingFunction 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
