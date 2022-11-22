49f7618b76f611b9eeaa62921c28e9a50bedfe9d
Samuel Matioli <smatioli@gmail.com>
2022-11-22 10:36:59 -0300

    Remove old version of the producer

--- golang-producer/run_all.sh
+++
@@ -1,5 +0,0 @@
-go run main.go ../data/org1-dev003-latlong.txt &
-go run main.go ../data/org1-dev003-speed.txt &
-go run main.go ../data/org1-dev004-latlong.txt &
-go run main.go ../data/org1-dev005-latlong.txt &
-go run main.go ../data/org1-dev006-latlong.txt &
